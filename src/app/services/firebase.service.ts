942f211894d7bc740154515ed7b82b055a0676f0
Carlos Fernandez <dominguezalacid@gmail.com>
2024-03-03 19:02:51 +0100

    actualizar

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -4,7 +4,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendPasswordResetEmail } from 'firebase/auth';
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { getFirestore, setDoc, doc, getDoc ,addDoc,collection, collectionData,query, updateDoc, deleteDoc} from '@angular/fire/firestore';
+import { getFirestore, setDoc, doc, getDoc, addDoc, collection, collectionData, query, updateDoc, deleteDoc } from '@angular/fire/firestore';
 import { UtilsService } from './utils.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { getStorage, uploadString, ref, getDownloadURL, deleteObject } from 'firebase/storage';
@@ -17,14 +17,17 @@ import { QueryConstraint, getDocs } from 'firebase/firestore';
 })
 export class FirebaseService {
 
-  auth = inject(AngularFireAuth);
-  firestore = inject(AngularFirestore);
-  storage = inject(AngularFireStorage);
-  utilsSvc = inject(UtilsService);
+  constructor(
+    private utilsSvc: UtilsService,
+    private firestore: AngularFirestore,
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth
+
+  ) { }
+
 
   //AUTENTICACION FIREBASE
-
-  getAuth(){
+  getAuth() {
     return getAuth();
   }
 
@@ -44,7 +47,7 @@ export class FirebaseService {
   }
 
   // OBTENER TODOS LOS USUARIOS
-  getAllUsers():Observable<any[]> {
+  getAllUsers(): Observable<any[]> {
     const path = 'users';
     return this.getCollectionData(path);
   }
@@ -58,7 +61,7 @@ export class FirebaseService {
     return sendPasswordResetEmail(getAuth(), email);
   }
 
-  singOut(){
+  singOut() {
     getAuth().signOut();
     localStorage.removeItem('user');
     this.utilsSvc.routerLink('/auth');
@@ -67,17 +70,13 @@ export class FirebaseService {
   //BASE DE DATOS
 
   //CREAR UNA COLECCION
- async createCollection(path: string, data: any): Promise<any> {
+  async createCollection(path: string, data: any): Promise<any> {
     const firestore = getFirestore();
     const docRef = await addDoc(collection(firestore, path), data);
     return docRef;
   }
 
   //OBTENER UNA COLECCION
-  // getCollectionData(path:string, collectionQuery?:any){
-  //   const ref = collection(getFirestore(),path);
-  //   return collectionData(query(ref,...collectionQuery),{idField:'id'});
-  // }
   getCollectionData(path: string, collectionQuery?: QueryConstraint[]): Observable<any[]> {
     const ref = collection(getFirestore(), path);
 
@@ -99,7 +98,7 @@ export class FirebaseService {
   }
 
   //ELIMINAR UN DOCUMENTO
-  deleteDocument(path: string){
+  deleteDocument(path: string) {
     return deleteDoc(doc(getFirestore(), path));
   }
 
@@ -108,68 +107,68 @@ export class FirebaseService {
   }
 
   //AGREGAR UN DOCUMENTO
-  addDocument(path: string, data: any):Promise<any> {
+  addDocument(path: string, data: any): Promise<any> {
     return addDoc(collection(getFirestore(), path), data);
   }
 
   //ALMACENAMIENTO DE DATOS
 
   //SUBIR IMAGEN
-  async uploadImage(path:string, data_url:string){
-    return uploadString(ref(getStorage(),path),data_url,'data_url').then(()=>{
-      return getDownloadURL(ref(getStorage(),path));
+  async uploadImage(path: string, data_url: string) {
+    return uploadString(ref(getStorage(), path), data_url, 'data_url').then(() => {
+      return getDownloadURL(ref(getStorage(), path));
     });
   }
 
   //OBTENER RUTA IMAGEN CON URL
-  async getFilePath(url:string){
-    return ref(getStorage(),url).fullPath;
+  async getFilePath(url: string) {
+    return ref(getStorage(), url).fullPath;
   }
 
   //ELIMINAR ARCHIVOS
-  deleteFile(path:string){
-    return deleteObject(ref(getStorage(),path));
+  deleteFile(path: string) {
+    return deleteObject(ref(getStorage(), path));
   }
 
   //GRUPOS
 
   // OBTENER TODOS LOS GRUPOS
-getAllGroups():Observable<Grupo[]> {
-  const path = 'grupos';
-  return this.getCollectionData(path);
-}
-
-// OBTENER UN GRUPO POR ID
-getGroupById(groupId: string) {
-  const path = `grupos/${groupId}`;
-  return this.getDocument(path);
-}
-
-// CREAR UN NUEVO GRUPO
-async createGroup(group: Grupo, user: User): Promise<any> {
-  try {
-    group.members = [user.uid];
-    group.createdAt = new Date();
+  getAllGroups(): Observable<Grupo[]> {
     const path = 'grupos';
-    const { id, ...groupWithoutId } = group;
-    return addDoc(collection(getFirestore(), path), groupWithoutId);
-
-  } catch (error) {
-    console.error('Error al crear el grupo:', error);
-    throw error;
+    return this.getCollectionData(path);
   }
-}
 
-//ACTUALIZAR UN GRUPO
-updateGroup(groupId: string, updatedGroup: Partial<Grupo>) {
-  const path = `grupos/${groupId}`;
-  return this.updateDocument(path, updatedGroup);
-}
+  // OBTENER UN GRUPO POR ID
+  getGroupById(groupId: string) {
+    const path = `grupos/${groupId}`;
+    return this.getDocument(path);
+  }
 
-//ELIMINAR UN GRUPO
-deleteGroup(groupId: string) {
-  const path = `grupos/${groupId}`;
-  return this.deleteDocument(path);
-}
+  // CREAR UN NUEVO GRUPO
+  async createGroup(group: Grupo, user: User): Promise<any> {
+    try {
+      group.members = [user.uid];
+      group.createdAt = new Date();
+      const path = 'grupos';
+      const { id, ...groupWithoutId } = group;
+      return addDoc(collection(getFirestore(), path), groupWithoutId);
+
+    } catch (error) {
+      console.error('Error al crear el grupo:', error);
+      throw error;
+    }
+  }
+
+  //ACTUALIZAR UN GRUPO
+  updateGroup(groupId: string, updatedGroup: Partial<Grupo>) {
+    const path = `grupos/${groupId}`;
+    return this.updateDocument(path, updatedGroup);
+  }
+
+  //ELIMINAR UN GRUPO
+  deleteGroup(groupId: string) {
+    const path = `grupos/${groupId}`;
+    return this.deleteDocument(path);
+  }
 
 }
